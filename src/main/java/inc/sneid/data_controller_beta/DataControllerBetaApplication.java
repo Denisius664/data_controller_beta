f7761165dd7a5dc05361507d37567c54506479e5
Denisius664 <denismir423@gmail.com>
2020-05-06 19:29:18 +1100

    First commit

---
+++ src/main/java/inc/sneid/data_controller_beta/DataControllerBetaApplication.java
@@ -0,0 +1,13 @@
+package inc.sneid.data_controller_beta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataControllerBetaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataControllerBetaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/inc/sneid/data_controller_beta/DataControllerBetaApplicationTests.java
@@ -0,0 +1,13 @@
+package inc.sneid.data_controller_beta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataControllerBetaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
